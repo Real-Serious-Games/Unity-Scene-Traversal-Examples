f40e76eb6cd1f0918bb95e24cc2a120b7b0bfcb4
Ashley Davis <ashley.davis@realseriousgames.com>
2016-07-22 14:06:18 +1000

    Example of enumerating all game objects.

---
+++ Assets/1. Enumerate all game objects/Enumerate_all_game_objects.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumerate_all_game_objects : MonoBehaviour
+{
+	void Start ()
+    {
+        GameObject[] allGameObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach (var gameObject in allGameObjects)
+        {
+            // ... Do something with the game object ...
+
+            Debug.Log(gameObject.name);
+        }
+    }
+}
