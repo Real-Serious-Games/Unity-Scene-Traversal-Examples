3a5f83c186f5ed1dd212d819680a0cdc36534630
Ashley Davis <ashley.davis@realseriousgames.com>
2016-07-22 14:19:29 +1000

    Added an example of identifying root nodes in the scene.

---
+++ Assets/Identify_root_nodes.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Identify_root_nodes : MonoBehaviour
+{
+
+	void Start ()
+    {
+        GameObject[] allGameObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach (var gameObject in allGameObjects)
+        {
+            if (gameObject.transform.parent == null)
+            {
+                // ... Do something with the root game object ...
+
+                Debug.Log(gameObject.name);
+            }
+        }
+    }
+}
