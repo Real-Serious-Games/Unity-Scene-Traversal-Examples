76bd272f60e2ea0590fdb18c38a3127606e12fe2
Ashley Davis <ashley.davis@realseriousgames.com>
2016-07-28 13:13:05 +1000

    Added scene traversal API and examples of its use.

---
+++ Assets/7. Scene Traversal API/Scene_Traversal_API_Examples.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using RSG.Scene.Query;
+
+public class Scene_Traversal_API_Examples : MonoBehaviour {
+
+	void Start () {
+
+        var sceneTraversal = new SceneTraversal();
+
+        Debug.Log("Enumerate root game objects:");
+
+        foreach (var gameObject in sceneTraversal.RootNodes())
+        {
+            Debug.Log(gameObject.name);
+        }
+
+        Debug.Log("Get parent game object:");
+
+        var parentGameObject = GameObject.Find("L2.B").Parent();
+        Debug.Log(parentGameObject.name);
+
+
+        Debug.Log("Get root game object:");
+
+        var rootGameObject = GameObject.Find("L2.B").RootObject();
+        Debug.Log(rootGameObject.name);
+
+        Debug.Log("Enumerate children:");
+
+        foreach (var childGameObject in GameObject.Find("L1.B").Children())
+        {
+            Debug.Log(childGameObject.name);
+        }
+
+        Debug.Log("Enumerate scene (pre-order):");
+
+        foreach (var gameObject in sceneTraversal.PreOrderHierarchy())
+        {
+            Debug.Log(gameObject.name);
+        }
+
+        Debug.Log("Enumerate scene (post-order):");
+
+        foreach (var gameObject in sceneTraversal.PreOrderHierarchy())
+        {
+            Debug.Log(gameObject.name);
+        }
+
+        Debug.Log("Enumerate descendents (all game objects in a sub-tree):");
+
+        foreach (var descendentGameObject in GameObject.Find("L1.B").Descendents())
+        {
+            Debug.Log(descendentGameObject.name);
+        }
+
+        Debug.Log("Enumerate ancestors of a game object:");
+
+        foreach (var ancestorGameObject in GameObject.Find("L3.B").Ancestors())
+        {
+            Debug.Log(ancestorGameObject.name);
+        }
+    }
+}
---
+++ Assets/Scene Traversal API/GameObjectExts.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace RSG.Scene.Query
+{
+    /// <summary>
+    /// Extensions to GameObject for scene traversal and query.
+    /// </summary>
+    public static class GameObjectExts
+    {
+        private static SceneTraversal sceneTraversal = new SceneTraversal();
+
+        //
+        // Get the root object of the game object
+        //
+        public static GameObject RootObject(this GameObject gameObject)
+        {
+            if (gameObject.transform.parent == null)
+            {
+                //
+                // The object itself is the root GameObject.
+                return gameObject;
+            }
+
+            var rootTransform = gameObject.transform.parent;
+            while (rootTransform.parent != null)
+            {
+                rootTransform = rootTransform.parent;
+            }
+
+            return rootTransform.gameObject;
+        }
+
+        //
+        // Get the distinct collection of root objects for the input collection of GameObjects.
+        //
+        public static IEnumerable<GameObject> RootObjects(this IEnumerable<GameObject> gameObjects)
+        {
+            return gameObjects
+                .Select(go => go.RootObject())
+                .Distinct();
+        }
+
+        /// <summary>
+        /// Retreive the parent of a game object.
+        /// </summary>
+        public static GameObject Parent(this GameObject gameObject)
+        {
+            var transform = gameObject.transform;
+            if (transform.parent == null)
+            {
+                return null;
+            }
+
+            return transform.parent.gameObject;
+        }
+
+        /// <summary>
+        /// Get the collection of children for a particular game object.
+        /// </summary>
+        public static IEnumerable<GameObject> Children(this GameObject parent)
+        {
+            return sceneTraversal.Children(parent);
+        }
+
+        /// <summary>
+        /// Get the collection of all child gameobjects for the input collection of game objects.
+        /// </summary>
+        public static IEnumerable<GameObject> Children(this IEnumerable<GameObject> source)
+        {
+            return source.SelectMany(go => go.Children());
+        }
+
+
+        /// <summary>
+        /// Get collection of all ancestors of a particular game object, starting with the immediate parent and working up to the root object.
+        /// </summary>
+        public static IEnumerable<GameObject> Ancestors(this GameObject parent)
+        {
+            return sceneTraversal.Ancestors(parent);
+        }
+
+        /// <summary>
+        /// Get collection of all descendents, the entire tree of children under a particular game object.
+        /// </summary>
+        public static IEnumerable<GameObject> Descendents(this GameObject parent)
+        {
+            return sceneTraversal.Descendents(parent);
+        }
+
+        /// <summary>
+        /// Get collection of all descendents of the input collection of game objects.
+        /// </summary>
+        public static IEnumerable<GameObject> Descendents(this IEnumerable<GameObject> source)
+        {
+            return source.SelectMany(go => go.Descendents());
+        }
+
+        /// <summary>
+        /// Traverse only all game objects breadth first.
+        /// </summary>
+        public static IEnumerable<GameObject> BreadthFirst(this GameObject parent)
+        {
+            return sceneTraversal.BreadthFirst(parent);
+        }
+
+        /// <summary>
+        /// Get the entire sub-hierarchy in pre-order under a particular game object.
+        /// </summary>
+        public static IEnumerable<GameObject> PreOrderHierarchy(this GameObject parent)
+        {
+            return sceneTraversal.PreOrderHierarchy(parent);
+        }
+
+        /// <summary>
+        /// Get the entire sub-hierarchy in post-order under a particular game object.
+        /// </summary>
+        public static IEnumerable<GameObject> PostOrderHierarchy(this GameObject parent)
+        {
+            return sceneTraversal.PostOrderHierarchy(parent);
+        }
+
+        /// <summary>
+        /// Get all leaf game objects in the hierarchy under a particular game object.
+        /// </summary>
+        public static IEnumerable<GameObject> HierarchyLeafNodes(this GameObject parent)
+        {
+            return sceneTraversal.HierarchyLeafNodes(parent);
+        }
+    }
+}
